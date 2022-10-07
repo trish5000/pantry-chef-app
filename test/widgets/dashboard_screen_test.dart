45a4aee42fbf7a8c98196d09f09551909a4b1da9
Michelle Tolfa <michelle.tolfa@freshconsulting.com>
2022-10-07 08:48:27 -0700

    Fix dashboard screen test

--- test/widgets/dashboard_screen_test.dart
+++ test/widgets/dashboard_screen_test.dart
@@ -1,15 +1,21 @@
 import 'package:beamer/beamer.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:pantry_chef_app/dashboard/screens/dashboard_screen.dart';
-import 'package:pantry_chef_app/dashboard/widgets/dashboard_tab.dart';
+import 'package:pantry_chef_app/pantry/services/pantry_service.dart';
 
+import '../fakes/fake_classes.dart';
+import '../fakes/mock_services.dart';
 import 'simple_material_app_widget.dart';
 
 void main() {
   testWidgets('Dashboard screen - render smoke test',
       (WidgetTester tester) async {
+    final mockPantryService = MockPantryService();
+    when(() => mockPantryService.getPantry())
+        .thenAnswer((invocation) => Future(() => fakePantry()));
+
     final router = BeamerDelegate(
       locationBuilder: RoutesLocationBuilder(
         routes: {
@@ -20,7 +26,7 @@ void main() {
 
     await tester.pumpWidget(
       SimpleMaterialAppWidget(
-        overrides: const [],
+        overrides: [pantryServiceProvider.overrideWithValue(mockPantryService)],
         child: MaterialApp.router(
           routeInformationParser: BeamerParser(),
           routerDelegate: router,
@@ -31,21 +37,44 @@ void main() {
     router.beamToNamed('/');
     await tester.pumpAndSettle();
 
-    for (var i = 0; i < 5; i++) {
-      final tab = find.byWidgetPredicate(
-        (widget) => widget is DashboardTab && widget.tabIndex == i,
-      );
-      expect(tab, findsOneWidget);
-
-      final otherTabs = find.byWidgetPredicate(
-          (widget) => widget is DashboardTab && widget.tabIndex != i);
-      expect(otherTabs, findsNWidgets(4));
-    }
-
-    router.beamToNamed('/home');
+    final homeTab = find.text('HOME');
+    expect(homeTab, findsOneWidget);
+    await tester.tap(homeTab);
     await tester.pumpAndSettle();
 
-    final homeText = find.textContaining('HOME');
+    final homeText = find.textContaining('HOME SCREEN');
     expect(homeText, findsOneWidget);
+
+    final pantryTab = find.text('PANTRY');
+    expect(pantryTab, findsOneWidget);
+    await tester.tap(pantryTab);
+    await tester.pumpAndSettle();
+
+    final pantryText = find.textContaining('My Pantry');
+    expect(pantryText, findsOneWidget);
+
+    final profileTab = find.text('PROFILE');
+    expect(profileTab, findsOneWidget);
+    await tester.tap(profileTab);
+    await tester.pumpAndSettle();
+
+    final profileText = find.textContaining('PROFILE SCREEN');
+    expect(profileText, findsOneWidget);
+
+    final libraryTab = find.text('RECIPE LIBRARY');
+    expect(libraryTab, findsOneWidget);
+    await tester.tap(libraryTab);
+    await tester.pumpAndSettle();
+
+    final libraryText = find.textContaining('RECIPE LIBRARY');
+    expect(libraryText, findsOneWidget);
+
+    final exploreTab = find.text('EXPLORE');
+    expect(exploreTab, findsOneWidget);
+    await tester.tap(exploreTab);
+    await tester.pumpAndSettle();
+
+    final exploreText = find.textContaining('Explore SCREEN');
+    expect(exploreText, findsOneWidget);
   });
 }
